73bc5d70787174c650cea41938614fd430d8a06c
jrbigmon <junioterriaga0@gmail.com>
2022-09-06 21:36:02 -0300

    Instaled dotenv and configured global variable

--- src/database/config.js
+++ src/database/config.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 module.exports = {
-  "username": "root",
-  "password": null,
-  "database": "database_development",
-  "host": "127.0.0.1",
-  "dialect": "mysql"
+  "username": env.process.DB_USER,
+  "password": env.process.DB_PASS,
+  "database": env.process.DB_NAME,
+  "host": env.process.DB_HOST,
+  "dialect": env.process.DB_DIALECT,
+  'port': env.process.DB_PORT
 }
