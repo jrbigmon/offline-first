0adb7c9724b5a286727433ba8bbcd88c03bb6bfc
jrbigmon <junioterriaga0@gmail.com>
2022-09-08 15:09:26 -0300

    Added router of order

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 const baseUrl = '/api/v1/offline'
+
 const SupervisorRouter = require('./src/router/SupervisorRouter')
+const OrderRouter = require('./src/router/OrderRouter')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(baseUrl, SupervisorRouter)
+app.use(baseUrl, OrderRouter)
 
 app.listen(port, () => console.log('listening on port ' + port))
