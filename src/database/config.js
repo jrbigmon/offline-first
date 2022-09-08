1a51c88137b5b9be0240f362379188234b1c2a11
jrbigmon <junioterriaga0@gmail.com>
2022-09-08 18:39:11 -0300

    Syntax

--- src/database/config.js
+++ src/database/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 module.exports = {
-  "username": process.env.DB_USER,
-  "password": process.env.DB_PASS,
-  "database": process.env.DB_NAME,
-  "host": process.env.DB_HOST,
-  "dialect": process.env.DB_DIALECT
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT
 }
