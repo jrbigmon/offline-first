1f1848697b45506be185b770d2354950c48cfc7f
jrbigmon <junioterriaga0@gmail.com>
2022-09-08 18:39:34 -0300

    Create middeware to bulkInsert

---
+++ src/middlewares/bulkInsertOrdersStaged.js
@@ -0,0 +1,14 @@
+const { Order } = require('../models')
+const fileManipulate = require('../stagedProcess/fileManipulate')
+
+const bulkInsertOrdersStaged = async (req, res, next) => {
+   try {
+        const ordersStaged = fileManipulate.open()
+        await Order.bulkCreate(ordersStaged)
+        fileManipulate.store([])
+        return next()
+   } catch (err) {
+        return next()
+   }
+}
+module.exports = bulkInsertOrdersStaged
