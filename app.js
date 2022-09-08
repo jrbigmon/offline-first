30864d5d05b58bec33fe4bf4bd164ec9bdf836cc
jrbigmon <junioterriaga0@gmail.com>
2022-09-08 18:38:12 -0300

    Added middleware bulkInsert in database after connection

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const baseUrl = '/api/v1/offline'
 
 const SupervisorRouter = require('./src/router/SupervisorRouter')
 const OrderRouter = require('./src/router/OrderRouter')
+const bulkInsertOrdersStaged = require('./src/middlewares/bulkInsertOrdersStaged')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use(bulkInsertOrdersStaged)
 app.use(baseUrl, SupervisorRouter)
 app.use(baseUrl, OrderRouter)
 
