3188c057175dd9319452a519e538956e5e0ccdcd
jrbigmon <junioterriaga0@gmail.com>
2022-09-08 18:38:39 -0300

    Created methods to new order

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -1,9 +1,30 @@
 const { Order } = require('../models')
+const orderOnHold = require('../stagedProcess/orderOnHold')
 
 const OrderController = {
-    createOrder: async (props) => {
-       
+    createOrder: async (req, res) => {
+        try {
+            const { geolocation, supervisorId } = req.body
+
+            const newOrder = { date: new Date(), geolocation, supervisorId: parseInt(supervisorId) }
+
+            const orderAfterCreate = await Order.create(newOrder)
+
+            return res.json(orderAfterCreate)
+        } catch (err) {
+            const { geolocation, supervisorId } = req.body
+                
+            const newOrder = { date: new Date(), geolocation, supervisorId }
+
+            const orderAfterStagedAdd = orderOnHold(newOrder)
+                
+            return res.json(orderAfterStagedAdd)
+        }
+    },
+    
+    getOrders: async (req, res) => {
+
     }
 }
-OrderController.createOrder()
+
 module.exports = OrderController
